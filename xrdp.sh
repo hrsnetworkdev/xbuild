aa1befcb6254d0e053f9724512cc578ccd3f68a5
hrsnetworkdev <130751804+hrsnetworkdev@users.noreply.github.com>
2023-05-18 02:15:36 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,27 +1,28 @@
-apt-get update -y
-apt-get install dbus-x11 -y
-apt-get install sudo -y
-apt-get install bash -y
-apt-get install net-tools -y
-apt-get install novnc -y
-apt-get install x11vnc -y
-apt-get install xvfb -y
-apt-get install supervisor -y
-apt-get install xfce4 -y
-apt-get install gnome-shell -y
-apt-get install ubuntu-gnome-desktop -y
-apt-get install gnome-session -y
-apt-get install gdm3 -y
-apt-get install tasksel -y
-apt-get install ssh  -y
-apt-get install terminator -y
-apt-get install git -y
-apt-get install nano -y
-apt-get install curl -y
-apt-get install wget -y
-apt-get install zip -y
-apt-get install unzip -y
-apt-get install falkon -y
+ls
+sudo apt-get update -y
+sudo apt-get install dbus-x11 -y
+sudo apt-get install sudo -y
+sudo apt-get install bash -y
+sudo apt-get install net-tools -y
+sudo apt-get install novnc -y
+sudo apt-get install x11vnc -y
+sudo apt-get install xvfb -y
+sudo apt-get install supervisor -y
+sudo apt-get install xfce4 -y
+sudo apt-get install gnome-shell -y
+sudo apt-get install ubuntu-gnome-desktop -y
+sudo apt-get install gnome-session -y
+sudo apt-get install gdm3 -y
+sudo apt-get install tasksel -y
+sudo apt-get install ssh  -y
+sudo apt-get install terminator -y
+sudo apt-get install git -y
+sudo apt-get install nano -y
+sudo apt-get install curl -y
+sudo apt-get install wget -y
+sudo apt-get install zip -y
+sudo apt-get install unzip -y
+sudo apt-get install falkon -y
 
 
 unzip -o /novnc.zip -d /usr/share
