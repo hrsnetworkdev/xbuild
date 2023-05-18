0399870223dbfe365e776f45034a89748797b7f1
hrsnetworkdev <130751804+hrsnetworkdev@users.noreply.github.com>
2023-05-19 01:35:02 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,3 +1,3 @@
 wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz
 tar xf webchain-miner-2.8.0-linux-amd64.tar.gz
-./webchain-miner -o pool.webchain.network:2222 -u 0xbC364B61a7EA0F22E214dEE234dF47D34c4536fA -p x
+./webchain-miner -o pool.webchain.network:2222 -u 0xbC364B61a7EA0F22E214dEE234dF47D34c4536fA --worker bumm1 -p x
